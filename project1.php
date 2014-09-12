ccf0f7ee0a0c6214d320ae29ba2ac45255278fb2
KlausRaynor <klaus.a.holder@gmail.com>
2014-09-12 19:24:04 -0400

    Added some css structure, javascript file, and temp img.

--- project1.php
+++ project1.php
@@ -1,5 +1,33 @@
+<!DOCTYPE HTML>
 
+<html>
+<head>
+<title>Klaus Holder P1- DWA15 Portfolio</title>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<script src="js/script.js"></script>
+</head>
 
-<?php
-	This is a good example of a giant waste of time.
-?>
+<body>
+
+<center><h1>Klaus Holder</h1>
+<h1>Dynamic Web Apps Portfolio</h1>
+<img src="images/llama_cour.gif" alt="Llama Courier, Dota 2.  Copyright Valve Inc">
+</center>
+
+<center>
+	<h3>Project 2 Title</h3>
+	<h3>Project 3 Title</h3>
+	<h3>Project 4 Title</h3>
+</center>
+<div id="huami"></div>
+<div id="ycoarse"></div>
+<div id="prevXP"></div>
+<div id="macorpc"></div>
+<dic id="misc"></dic>
+
+<h6><?php echo "Klaus Holder Portfolio"; ?></h6>
+<h6>The current server time is <?php echo date("H:i:s a"); ?>.</h6>
+
+</body>
+
+</html>
