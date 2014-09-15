eff7d5e4fffb98750895963298f49d05f84c665f
KlausRaynor <klaus.a.holder@gmail.com>
2014-09-15 10:35:19 -0400

    finished descriptions, added reqs into readme, added different style

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 Problem Set 1.  
 ---
 Created by Klaus Holder
+---
+
+Live URL: [Problem Set 1]: http://p1.klausholder.me
+
+###Description
+	Intro Problem Set 1.  Outlines a bit about myself and my goals in this course, as well as the years to come.  I've implemented some Javascript and CSS styling into the page, just for fun.  
+###Demo Information
+	No demo.
+###Plugins or Outside Libraries Used
+	N/A
--- project1.php
+++ project1.php
@@ -19,13 +19,13 @@
 	<h3>Project 3 Title</h3>
 	<h3>Project 4 Title</h3>
 </center>
-<div id="huami"></div>
-<div id="ycoarse"></div>
-<div id="prevXP"></div>
-<div id="macorpc"></div>
-<dic id="misc"></dic>
+<div class="info" id="huami"><h3>Who Am I?</h3>My name is Klaus Holder, current student pursuing a Bachelors of Liberal Arts at Harvard's Extension School.  Working part-time at Harvard's Graduate School of Education, I use my spare hours to program games in Unity with a small team.   Currently, we are working on creating a demo for a game titled "A Pack of Alpacas".  </div><br>
+<div class="info" id="ycoarse"><h3>Why I'm taking this course</h3><br> I decided to take dynamic web applications to know and understand more front about the whole picture of building/maintaining a website, as well as building one that looks and feels alive.  By far my favorite course this semester!</div><br>
+<div class="info" id="prevXP"><h3>Previous Experience</h3><br>My previous experience programming includes learning Java, working in a small startup on a blackberry app, teaching kids modding through minecraft (using Java), and building sites/small projects via Javascript.</div><br>
+<div class="info" id="macorpc"><h3>Mac or PC?</h3><br>Both.  I have a mac laptop and a custom built PC running Windows.  I've set up my dev environment on both and have edited this code on both as well!</div><br>
+<div class="info" id="misc"><h3>Misc</h3><br>I'm a huge gamer, passionate about the medium and always willing to talk your ear off about the intricate and beautiful story told by the Metal Gear Solid series.</div><br>
 
-<h6><?php echo "Klaus Holder Portfolio"; ?></h6>
+<h6><?php echo "Klaus Holder Portfolio 2014 &copy;"; ?></h6>
 <h6>The current server time is <?php echo date("H:i:s a"); ?>.</h6>
 
 </body>
