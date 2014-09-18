e05b7ecd45f8384e873640731a86254d6df23bb0
KlausRaynor <klaus.a.holder@gmail.com>
2014-09-18 16:03:59 -0400

    Finalized project 1.  Added a diff picture, changed CSS styling, and added required README.md elements.

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Created by Klaus Holder
 Live URL: [Problem Set 1]: http://p1.klausholder.me
 
 ###Description
-	Intro Problem Set 1.  Outlines a bit about myself and my goals in this course, as well as the years to come.  I've implemented some Javascript and CSS styling into the page, just for fun.  
+>Intro Problem Set 1.  Outlines a bit about myself and my goals in this course, as well as personal goals for the years to come.  I've implemented some CSS styling into the page, just for fun.  
 ###Demo Information
 	No demo.
 ###Plugins or Outside Libraries Used
--- project1.php
+++ project1.php
@@ -11,15 +11,20 @@
 
 <center><h1>Klaus Holder</h1>
 <h1>Dynamic Web Apps Portfolio</h1>
-<img src="images/llama_cour.gif" alt="Llama Courier, Dota 2.  Copyright Valve Inc">
+<img src="images/yourstruly3.jpeg" alt="Klaus in the House!">
 </center>
 
+<div id="menu">
 <center>
 	<h3>Project 2 Title</h3>
+	View:[Link] Github:[Link]
 	<h3>Project 3 Title</h3>
+	View:[Link] Github:[Link]
 	<h3>Project 4 Title</h3>
+	View:[Link] Github:[Link]
 </center>
-<div class="info" id="huami"><h3>Who Am I?</h3>My name is Klaus Holder, current student pursuing a Bachelors of Liberal Arts at Harvard's Extension School.  Working part-time at Harvard's Graduate School of Education, I use my spare hours to program games in Unity with a small team.   Currently, we are working on creating a demo for a game titled "A Pack of Alpacas".  </div><br>
+</div>
+<div class="info" id="huami"><h3>Who Am I?</h3>My name is Klaus Holder, current student pursuing a Bachelors of Liberal Arts at Harvard's Extension School.  Working part-time at Harvard's Graduate School of Education, I use my spare hours to program games in Unity with a small team.   Currently, we are working on creating a demo for a game still in progress.  </div><br>
 <div class="info" id="ycoarse"><h3>Why I'm taking this course</h3><br> I decided to take dynamic web applications to know and understand more front about the whole picture of building/maintaining a website, as well as building one that looks and feels alive.  By far my favorite course this semester!</div><br>
 <div class="info" id="prevXP"><h3>Previous Experience</h3><br>My previous experience programming includes learning Java, working in a small startup on a blackberry app, teaching kids modding through minecraft (using Java), and building sites/small projects via Javascript.</div><br>
 <div class="info" id="macorpc"><h3>Mac or PC?</h3><br>Both.  I have a mac laptop and a custom built PC running Windows.  I've set up my dev environment on both and have edited this code on both as well!</div><br>
